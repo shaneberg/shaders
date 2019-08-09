e1b8c9be04a22f1b6ceeb3ee0c0431c37e1589e3
Sean Haneberg <haneberg@gmail.com>
2019-08-08 18:50:43 -0700

    Add index.html

---
+++ src/index.js
@@ -0,0 +1,12 @@
+let THREE = require('three');
+
+function component() {
+    const element = document.createElement('div');
+
+      // Lodash, currently included via a script, is required for this line to work
+   element.innerHTML = 'test';
+   return element;
+
+}
+
+document.body.appendChild(component());
