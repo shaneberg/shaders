a2619012165997afb62637a62ff2bab355af2909
Sean Haneberg <haneberg@gmail.com>
2019-08-09 18:10:42 -0700

    wire up u_resolution to cube

--- src/index.js
+++ src/index.js
@@ -3,19 +3,29 @@ let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 let renderer = new THREE.WebGLRenderer();
 
-let shader = require('./glsl/test1.glsl');
+let shader = require('./glsl/algorithmic-drawing.glsl');
 
 function component() {
     let padding = 10;
     renderer.setSize(window.innerWidth - padding, window.innerHeight - padding);
     return renderer.domElement;
 }
+
 document.body.appendChild(component());
+
+let resolution = {
+    x: renderer.domElement.width,
+    y: renderer.domElement.height
+};
+
 let uniforms = {
     colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
     colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)},
     u_time: {type: 'f', value: 0.0},
+    u_resolution: {type: 'vec2', value: new THREE.Vector2()}
 };
+
+uniforms.u_resolution.value = resolution;
 
 let fragmentShader = () => {
     return shader;
