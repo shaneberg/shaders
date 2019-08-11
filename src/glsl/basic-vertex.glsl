7459e369effab7a7f9a0adc04101833dffc15b12
Sean Haneberg <haneberg@gmail.com>
2019-08-11 14:09:27 -0700

    vertex & camera

---
+++ src/glsl/basic-vertex.glsl
@@ -0,0 +1,7 @@
+#ifdef GL_ES
+precision mediump float;
+#endif
+
+void main() {
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ let scene = new THREE.Scene();
 let camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 let renderer = new THREE.WebGLRenderer();
 
-let shader = require('./glsl/algorithmic-drawing.glsl');
+let vertexShader = require('./glsl/basic-vertex.glsl');
+let fragmentShader = require('./glsl/algorithmic-drawing.glsl');
 
 function component() {
     let padding = 10;
@@ -25,7 +26,11 @@ uniforms.u_resolution.value.x = renderer.domElement.width * 0.7;
 uniforms.u_resolution.value.y = renderer.domElement.height * 0.7;
 
 let geometry = new THREE.PlaneGeometry();
-let material =  new THREE.ShaderMaterial({ uniforms: uniforms, fragmentShader: shader });
+let material =  new THREE.ShaderMaterial({
+    uniforms: uniforms,
+    vertexShader,
+    fragmentShader,
+});
 
 let plane = new THREE.Mesh( geometry, material );
 scene.add( plane );
@@ -35,6 +40,7 @@ camera.position.z = 0.7;
 function animate() {
     uniforms.u_time.value += 0.05;
 
+    camera.position.z +=  0.001;
     requestAnimationFrame( animate );
     renderer.render( scene, camera );
 }
